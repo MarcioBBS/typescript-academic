e642cc0b3dc23a7815e99e711971aaae06194ce4
MacioBBS <marciobbs@gmail.com>
2021-05-18 11:33:33 -0700

    Assign Type Aliases

--- with-typescript.js
+++ with-typescript.js
@@ -5,10 +5,19 @@ var result = addNum(2, 5);
 console.log(result);
 var hobbies = ['Surf', 'Snowboard', 'Cooking'];
 console.log(hobbies);
+// Set the var person to the type Person
 var person;
-person = [
-    { name: 'Marcio', age: 42 },
-    { name: 'Leah', age: 38 },
-    { name: 'Julian', age: 2 }
-];
+person = {
+    name: 'Marcio',
+    age: 42
+};
+// Set the var people to the type Person
+var people;
 console.log(person);
+// INFERENCE
+var course = 'Vai se fuder';
+course = 'Vai voce meu fio';
+//course = 1233;  This will not work as the course is inferenced as a string value.
+// UNION TYPES
+var courseUnion = 'Vai que vai!';
+courseUnion = 123;
